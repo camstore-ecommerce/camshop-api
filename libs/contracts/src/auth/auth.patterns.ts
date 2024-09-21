b695df3094a0592d50b77888852532a53f615a6d
d3m0l15h <davicmax123@gmail.com>
2024-09-21 10:05:33 +0700

    feat(libs/contracts): auth dto

---
+++ libs/contracts/src/auth/auth.patterns.ts
@@ -0,0 +1,6 @@
+export const AUTH_PATTERNS = {
+    USER_LOGIN: 'users.auth.user_login',
+    USER_REGISTER: 'users.auth.user_register',
+    ADMIN_LOGIN: 'users.auth.admin_login',
+    AUTHENTICATE: 'users.auth.authenticate',
+}
---
+++ libs/contracts/src/auth/index.ts
@@ -0,0 +1,4 @@
+export * from './admin-login.dto';
+export * from './auth.patterns';
+export * from './user-register.dto';
+export * from './user-login.dto';
