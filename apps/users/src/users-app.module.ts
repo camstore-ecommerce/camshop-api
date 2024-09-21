02de97b49f03acbf0104f92c4db94c2dbf3e527f
d3m0l15h <davicmax123@gmail.com>
2024-09-21 09:56:47 +0700

    fix(micro-user): change from typeorm to prisma

--- apps/users/src/users-app.module.ts
+++ apps/users/src/users-app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import * as Joi from 'joi';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
 	imports: [
@@ -15,30 +14,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 					.valid('development', 'production', 'test', 'provision', 'staging')
 					.default('development'),
 				PORT: Joi.number(),
-				POSTGRES_HOST: Joi.string().required(),
-				POSTGRES_PORT: Joi.number().required(),
-				POSTGRES_USER: Joi.string().required(),
-				POSTGRES_PASSWORD: Joi.string().required(),
-				POSTGRES_DB: Joi.string().required(),
+				DATABASE_URL: Joi.string().required(),
+				JWT_SECRET: Joi.string().required(),
+				JWT_EXPIRATION: Joi.number().required(),
 			}),
 			validationOptions: {
 				abortEarly: true,
 			},
 			cache: true,
 			expandVariables: true,
-		}),
-		TypeOrmModule.forRootAsync({
-			useFactory: (configService: ConfigService) => ({
-				type: 'postgres',
-				host: configService.getOrThrow('POSTGRES_HOST'),
-				port: configService.getOrThrow('POSTGRES_PORT'),
-				username: configService.getOrThrow('POSTGRES_USER'),
-				password: configService.getOrThrow('POSTGRES_PASSWORD'),
-				database: configService.getOrThrow('POSTGRES_DB'),
-				synchronize: true,
-				autoLoadEntities: true,
-			}),
-			inject: [ConfigService],
 		}),
 		UsersModule,
 		AuthModule,
