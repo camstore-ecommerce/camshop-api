80b6a1932b1dc379f73568040878c9976b662261
d3m0l15h <davicmax123@gmail.com>
2024-10-22 08:04:00 +0700

    format

--- apps/api-gateway/src/cdn/cdn.service.spec.ts
+++ apps/api-gateway/src/cdn/cdn.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CdnService } from './cdn.service';
 
 describe('CdnService', () => {
-  let service: CdnService;
+	let service: CdnService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CdnService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [CdnService],
+		}).compile();
 
-    service = module.get<CdnService>(CdnService);
-  });
+		service = module.get<CdnService>(CdnService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
