16c720658d9c6899fa306753e3a47c68c08df0f3
d3m0l15h <davicmax123@gmail.com>
2024-11-11 15:39:10 +0700

    feat(contracts): orders

--- libs/contracts/src/orders/index.ts
+++ libs/contracts/src/orders/index.ts
@@ -1,3 +1,1 @@
-export * from './create-order.dto';
-export * from './update-order.dto';
-export * from './order.patterns';
+export * from './orders';
--- libs/contracts/src/orders/order.patterns.ts
+++
@@ -1,8 +0,0 @@
-export const ORDER_PATTERNS = {
-	CREATE: 'orders.create',
-	FIND_ALL: 'orders.findAll',
-	FIND_ONE: 'orders.findOne',
-	UPDATE: 'orders.update',
-	REMOVE: 'orders.remove',
-	PERMANENTLY_REMOVE: 'orders.permanentlyRemove',
-};
