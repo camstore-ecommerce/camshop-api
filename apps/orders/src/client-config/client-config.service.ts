0159fa496a7020d84eefe33186553463a82657d6
d3m0l15h <davicmax123@gmail.com>
2024-11-11 15:48:59 +0700

    feat(orders_service): client-config module

---
+++ apps/orders/src/client-config/client-config.service.ts
@@ -0,0 +1,50 @@
+import { PRODUCTS_PACKAGE_NAME } from '@app/contracts/products';
+import { USERS_PACKAGE_NAME } from '@app/contracts/users';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientOptions, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class ClientConfigService {
+	constructor(private configService: ConfigService) {}
+
+	get productsClientOption(): ClientOptions {
+		return {
+			transport: Transport.GRPC,
+			options: {
+				package: PRODUCTS_PACKAGE_NAME,
+				protoPath: [
+					'proto/products/products.proto',
+					'proto/products/manufacturers.proto',
+					'proto/products/categories.proto',
+				],
+				url: this.configService.get('PRODUCTS_CLIENT_URL') || '0.0.0.0:50051',
+				loader: {
+					keepCase: true,
+					longs: String,
+					enums: String,
+					defaults: true,
+					oneofs: true,
+				},
+			},
+		};
+	}
+
+	get usersClientOption(): ClientOptions {
+		return {
+			transport: Transport.GRPC,
+			options: {
+				package: USERS_PACKAGE_NAME,
+				protoPath: ['proto/users/users.proto', 'proto/users/auth.proto'],
+				url: this.configService.get('USERS_CLIENT_URL') || '0.0.0.0:50052',
+				loader: {
+					keepCase: true,
+					longs: String,
+					enums: String,
+					defaults: true,
+					oneofs: true,
+				},
+			},
+		};
+	}
+}
