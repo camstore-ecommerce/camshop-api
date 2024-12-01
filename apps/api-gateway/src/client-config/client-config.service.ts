7fef977885ae1e4d5ddf9f6af816600c8fe33792
d3m0l15h <davicmax123@gmail.com>
2024-12-01 21:35:11 +0700

    refactor(api-gateway): refactor for env config, add cart config

--- apps/api-gateway/src/client-config/client-config.service.ts
+++ apps/api-gateway/src/client-config/client-config.service.ts
@@ -1,3 +1,4 @@
+import { CART_PACKAGE_NAME } from '@app/contracts/cart';
 import { ORDERS_PACKAGE_NAME } from '@app/contracts/orders';
 import { PRODUCTS_PACKAGE_NAME } from '@app/contracts/products';
 import { USERS_PACKAGE_NAME } from '@app/contracts/users';
@@ -21,14 +22,6 @@ export class ClientConfigService {
 		return this.configService.get<string>('CDN_API_SECRET');
 	}
 
-	getUsersClientPort(): number {
-		return this.configService.get<number>('USERS_CLIENT_PORT');
-	}
-
-	getOrdersClientPort(): number {
-		return this.configService.get<number>('ORDERS_CLIENT_PORT');
-	}
-
 	get productsClientOption(): ClientOptions {
 		return {
 			transport: Transport.GRPC,
@@ -39,7 +32,7 @@ export class ClientConfigService {
 					'proto/products/manufacturers.proto',
 					'proto/products/categories.proto',
 				],
-				url: this.configService.get('PRODUCTS_CLIENT_URL') || '0.0.0.0:50051',
+				url: this.configService.get('PRODUCTS_CLIENT_URL'),
 				loader: {
 					keepCase: true,
 					longs: String,
@@ -61,7 +54,7 @@ export class ClientConfigService {
 					'proto/users/auth.proto', 
 					'proto/users/addresses.proto'
 				],
-				url: this.configService.get('USERS_CLIENT_URL') || '0.0.0.0:50052',
+				url: this.configService.get('USERS_CLIENT_URL'),
 				loader: {
 					keepCase: true,
 					longs: String,
@@ -79,7 +72,25 @@ export class ClientConfigService {
 			options: {
 				package: ORDERS_PACKAGE_NAME,
 				protoPath: ['proto/orders/orders.proto'],
-				url: this.configService.get('ORDERS_CLIENT_URL') || '0.0.0.0:50053',
+				url: this.configService.get('ORDERS_CLIENT_URL'),
+				loader: {
+					keepCase: true,
+					longs: String,
+					enums: String,
+					defaults: true,
+					oneofs: true,
+				},
+			},
+		};
+	}
+
+	get cartClientOption(): ClientOptions {
+		return {
+			transport: Transport.GRPC,
+			options: {
+				package: CART_PACKAGE_NAME,
+				protoPath: ['proto/cart/cart.proto'],
+				url: this.configService.get('CART_CLIENT_URL'),
 				loader: {
 					keepCase: true,
 					longs: String,
