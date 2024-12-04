d45b301e38425eecd7efbd20b2ac416ace18098f
d3m0l15h <davicmax123@gmail.com>
2024-12-04 21:18:37 +0700

    fix(orders_micro): remove forRoot() config

--- apps/orders/src/client-config/client-config.module.ts
+++ apps/orders/src/client-config/client-config.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientConfigService } from './client-config.service';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			envFilePath: `./apps/orders/${process.env.NODE_ENV === 'development' ? '.env.dev' : '.env'}`,
-			validationSchema: Joi.object({
-				NODE_ENV: Joi.string()
-					.valid('development', 'production', 'test', 'provision', 'staging')
-					.default('development'),
-			}),
-			validationOptions: {
-				abortEarly: true,
-			},
-		}),
-	],
+	imports: [ConfigModule],
 	providers: [ClientConfigService],
 	exports: [ClientConfigService],
 })
