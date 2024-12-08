c086213608c5ac8f8bf33d9dfefbb7f748aa2be1
d3m0l15h <davicmax123@gmail.com>
2024-12-08 22:04:04 +0700

    document(api-gateway): add documents

--- apps/api-gateway/src/inventory/inventory.controller.ts
+++ apps/api-gateway/src/inventory/inventory.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { JwtAuthGuard } from '@app/common/guards';
-import { CreateInventoryDto, UpdateInventoryDto, UpdateReservedStockDto } from '@app/contracts/inventory';
+import { CreateInventoryDto, Inventories, Inventory, UpdateInventoryDto, UpdateReservedStockDto } from '@app/contracts/inventory';
 import { Public, Roles } from '@app/common/decorators';
 import { Role } from '@app/common/enums';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('inventory')
 @UseGuards(JwtAuthGuard)
@@ -12,40 +13,54 @@ export class InventoryController {
 
   @Post()
   @Roles(Role.Admin)
+  @ApiOperation({ summary: 'Create inventory', description: 'Admin access' })
+  @ApiResponse({ status: 201, type: Inventory })
   create(@Body() createInventoryDto: CreateInventoryDto) {
     return this.inventoryService.create(createInventoryDto);
   }
 
   @Patch(':id')
   @Roles(Role.Admin)
+  @ApiOperation({ summary: 'Update inventory', description: 'Admin access' })
+  @ApiResponse({ status: 200, type: Inventory })
   update(@Param('id') id: string, @Body() updateInventoryDto: UpdateInventoryDto) {
     return this.inventoryService.update(id, updateInventoryDto);
   }
 
   @Get('products/:id')
   @Public()
+  @ApiOperation({ summary: 'Find inventory by product', description: 'Public' })
+  @ApiResponse({ status: 200, type: Inventory })
   findByProduct(@Param('id') id: string) {
     return this.inventoryService.findByProduct(id);
   }
 
   @Get()
   @Public()
+  @ApiOperation({ summary: 'Find all inventory', description: 'Public' })
+  @ApiResponse({ status: 200, type: Inventories })
   findAll() {
     return this.inventoryService.findAll();
   }
 
   @Patch('reserve-stock')
+  @ApiOperation({ summary: 'Reserve stock', description: 'Public' })
+  @ApiResponse({ status: 200, type: Inventory })
   reserveStock(@Body() updateReservedStockDto: UpdateReservedStockDto) {
     return this.inventoryService.reserveStock(updateReservedStockDto);
   }
 
   @Patch('release-stock')
+  @ApiOperation({ summary: 'Release stock', description: 'Public' })
+  @ApiResponse({ status: 200, type: Inventory })
   releaseStock(@Body() updateReservedStockDto: UpdateReservedStockDto) {
     return this.inventoryService.releaseStock(updateReservedStockDto);
   }
 
   @Get(':id')
   @Public()
+  @ApiOperation({ summary: 'Find one inventory', description: 'Public' })
+  @ApiResponse({ status: 200, type: Inventory })
   findOne(@Param('id') id: string) {
     return this.inventoryService.findOne(id);
   }
